89f3f74d529df4d77df814fc917d06e671b3e80c
Anton Olsson <141565741+AntonOlsson82@users.noreply.github.com>
2025-12-18 23:44:20 +0100

    Very simple menu
    A very simple menu and testing to see if I can commit and push everything correctly.

--- gymLogC/main.c
+++ gymLogC/main.c
@@ -3,6 +3,40 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int input = 1;
+    char input1[16];
+
+    while(input != 0)
+    {
+
+        printf("*----Gym Logging----*\n");
+        printf("1. Start new workout\n");
+        printf("2. View previous workouts\n");
+        printf("3. Add new exercise\n");
+        printf("4. View workout statistics\n");
+
+        fgets(input1, sizeof(input1), stdin);
+        input = atoi(input1);
+
+        switch(input)
+        {
+        case 1:
+            printf("1");
+            break;
+        case 2:
+            printf("2");
+            break;
+        case 3:
+            printf("3");
+            break;
+        case 4:
+            printf("4");
+            break;
+        case 0:
+            printf("Quiting...");
+        }
+
+    }
+
     return 0;
 }
